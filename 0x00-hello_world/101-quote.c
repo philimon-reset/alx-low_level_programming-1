fecf6c815bde23382c846e4d2113bfef3687523f
Doro-000 <hizkyastekle@gmail.com>
2021-02-03 16:24:55 +0000

    solution for advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point, prints a string to stderr
+ *
+ * Return : returns 1(fail)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\""
+		"- Dora korpar, 2015-10-19\n");
+	return (1);
+}
