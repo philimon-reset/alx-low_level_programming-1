5defb2ab478e1f68cceff4f19194ab90ec06916a
Doro-000 <hizkyastekle@gmail.com>
2021-02-03 18:18:08 +0000

    solution for advanced task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
 /**
  * main - entry point, prints a string to stderr
  *
- * Return : returns 1(fail)
+ * Return: returns 1(fail)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\""
-	      " - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
