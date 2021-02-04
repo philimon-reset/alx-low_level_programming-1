ba8995cefe9db5ddb5e6bdb8d7283c0892da3f49
Doro-000 <hizkyastekle@gmail.com>
2021-02-04 17:44:20 +0000

    solution for advanced task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int c, c_two, c_three, c_four;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (c_two = 48; c_two <= 57; c_two++)
+		{
+			if (c != 57 || c != 57)
+			{
+				for (c_three = 48; c_three <= 57; c_three++)
+				{
+					for (c_four = 48; c_four = 57; c_four++)
+					{
+						if (c_three != 48 || c_three != 48)
+						{
+							putchar(c);
+							putchar(c_two);
+							putchar(c_three);
+							if (c != 55 || c_two != 56 || c_three != 57)
+							{
+								putchar(44);
+								putchar(32);
+							}
+						}
+					}
+				}
+			}
+			else
+				break;
+		}
+	}
+
+	putchar(10);
+	return (0);
+}
