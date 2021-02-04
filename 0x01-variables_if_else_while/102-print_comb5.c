213b127cb014e25b80a38e83431a2908a580b062
Doro-000 <hizkyastekle@gmail.com>
2021-02-04 21:04:00 +0000

    update solution

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	int c, c_two;
-	
+	int c, c_two
+
 	for (c = 0; c <= 98; c++)
 	{
 		for (c_two = c + 1; c_two <= 99; c_two++)
-		{	
+		{
 			putchar('0' + c / 10);
 			putchar('0' + c % 10);
 			putchar(32);
 			putchar('0' + c_two / 10);
 			putchar('0' + c_two % 10);
 			if (c / 10 != 9 || c % 10 != 8)
-			{				
+			{
 				putchar(44);
 			}
 		}
