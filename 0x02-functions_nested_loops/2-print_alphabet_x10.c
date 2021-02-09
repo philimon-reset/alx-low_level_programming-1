ae2791a1088725c2ff57e380c094de75d363db22
Doro-000 <hizkyastekle@gmail.com>
2021-02-09 08:27:10 +0000

    update solution

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,15 +10,12 @@ void print_alphabet_x10(void)
 	char i;
 	int j;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
 		{
 			_putchar(i);
 		}
-		if (j < 10)
-		{
-			_putchar(10);
-		}
+		_putchar('\n');
 	}
 }
