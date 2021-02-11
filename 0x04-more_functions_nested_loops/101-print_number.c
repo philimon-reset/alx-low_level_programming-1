39fe9e10fcf3b12bd0d0ede426c81d43500fc376
Doro-000 <hizkyastekle@gmail.com>
2021-02-11 06:54:07 +0000

    solutioon for advanced task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints a number using _putchar
+ * @n: number to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int counter = 0, tmp, i, flag = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		if (n < 0)
+		{
+			n *= -1;
+			flag = 1;
+		}
+
+		tmp = n;
+
+		while (tmp)
+		{
+			tmp /= 10;
+			counter++;
+		}
+
+		if (flag)
+		{
+			_putchar('-');
+		}
+
+		for (i = counter - 1; i >= 0; i--)
+		{
+			_putchar(((n / (int) pow((double) 10, (double) i)) % 10) = '0');
+		}
+	}
+}
