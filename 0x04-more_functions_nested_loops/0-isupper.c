e8ffd64e817282c3cf1a03b191c4081f9c024a28
Doro-000 <hizkyastekle@gmail.com>
2021-02-11 02:55:10 +0000

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+/**
+ * _isupper - checks whether a char is uppercase or not
+ * @c: the char to be checked
+ *
+ * Return: 1 if c is uppercase
+ * 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies 2 numbers
+ * @a: first number to be computed
+ * @b: second numbert to be computed
+ *
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
